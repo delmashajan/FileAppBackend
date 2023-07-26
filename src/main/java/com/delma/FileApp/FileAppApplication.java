f165bbfd1acf570963d60fec06b29831dda79414
delmashajan <delmashajan@gmail.com>
2023-07-26 10:03:23 +0530

    swagger documentation done

--- src/main/java/com/delma/FileApp/FileAppApplication.java
+++ src/main/java/com/delma/FileApp/FileAppApplication.java
@@ -2,6 +2,7 @@ package com.delma.FileApp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 public class FileAppApplication {
